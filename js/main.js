055f9474f0e7a13d15692452cca09d8248363e77
tiffany-philippi <tiffanyphilippi15@gmail.com>
2023-01-19 17:41:34 -0300

    refactoring classes based on BEM CSS architecture

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 function toggleMenu() {
-  document.querySelector(".petinn-topnav").classList.toggle("responsive");
+  console.log("click");
+  document
+    .querySelector(".petinnHeader__topnav")
+    .classList.toggle("responsive");
+  console.log(document.querySelector(".petinnHeader__topnav"));
 }
+const benefits = [
+  "Diminui o risco da ansiedade de separação",
+  "Ambiente familiar",
+  "Quintal protegido para brincar",
+  "Fotos e vídeos durante a estadia para diminuir a saudade do tutor",
+  "Muito carinho ao longo do dia",
+];
