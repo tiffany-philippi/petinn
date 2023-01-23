381147f2798b737bd9baeb109ac98e3beff14040
tiffany-philippi <tiffanyphilippi15@gmail.com>
2023-01-23 17:04:18 -0300

    blog pages included

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-function toggleMenu() {
-  document
-    .querySelector(".petinnHeader__topnav")
-    .classList.toggle("responsive");
-  console.log(document.querySelector(".petinnHeader__topnav"));
-}
-const benefits = [
-  "Diminui o risco da ansiedade de separação",
-  "Ambiente familiar",
-  "Quintal protegido para brincar",
-  "Fotos e vídeos durante a estadia para diminuir a saudade do tutor",
-  "Muito carinho ao longo do dia",
-];
-
 var carouselDepositions = document.querySelector(".carousel__depositions");
 var flktyDepositions = new Flickity(carouselDepositions, {
   imagesLoaded: true,
